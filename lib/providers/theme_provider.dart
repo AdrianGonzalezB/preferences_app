096a243aede48b308126ee70141e37bff01d36fd
AdrianGonzalezB <adriangonzalez@paucasesnovescifp.cat>
2022-02-14 20:16:16 +0100

    Initial commit

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData currenTheme;
+
+  ThemeProvider({required bool isDarkMode})
+      : currenTheme = isDarkMode ? ThemeData.dark() : ThemeData.light();
+
+  setLightMode() {
+    currenTheme = ThemeData.light();
+    notifyListeners();
+  }
+
+  setDarkMode() {
+    currenTheme = ThemeData.dark();
+    notifyListeners();
+  }
+}
